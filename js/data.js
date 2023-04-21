d618203c4654667f130e3b851dfb824604b5527a
YX-Wang5 <122315407+YX-Wang5@users.noreply.github.com>
2023-04-21 12:02:56 -0400

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,32 @@
+window.reviewData = [
+  {
+    name: "Henry Huang",
+    date: new Date("2023-04-5"),
+    rating: 4,
+    review: "I love playing guess who with my friends it's a fun game that make us laugh alot but my friends could always guess my person faster than I could."
+  },
+  {
+    name: "Calvin Zhou",
+    date: new Date("2023-04-8"),
+    rating: 4,
+    review: "This game is fun to enjoy with my family but it could be better if there is more person cards you can switch out so the game can be more fun."
+  },
+  {
+    name: "Peter Parker",
+    date: new Date("2023-04-8"),
+    rating: 5,
+    review: "Amazing game to play at a game night."
+  },
+  { 
+    name: "Walter White",
+    date: new Date("2023-04-10"),
+    rating: 1,
+    review: "This game is way too easy for me, wish it can be more challanging so II don't keep on beating my friends so easily."
+  },
+  {
+    name: "Stan Smith",
+    date: new Date("2023-04-12"),
+    rating: 5,
+    review: "Love this game, love seeing my kids ask smart questions to beat each other and it's helping them learn things aswell."
+  }
+];
